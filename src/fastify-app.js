ddacf0d126847f1ceaeebc0803bce37d58ab94bb
Glib Rulev <56866680+velzepooz@users.noreply.github.com>
2023-03-10 17:58:52 +0200

    add general http error handling and pino-pretty for dev mode (#5)

--- src/fastify-app.js
+++ src/fastify-app.js
@@ -2,6 +2,8 @@
 
 import fastify from 'fastify';
 import fastifyCookie from '@fastify/cookie';
+
+import { httpErrorHandler } from './utils/error-handlers.utils.js';
 import { initControllers } from './init-controllers.js';
 import container from './di-container.js';
 
@@ -13,6 +15,7 @@ export const initFastifyApp = (opts = { logger: true }) => {
   const app = fastify(opts);
 
   app.register(fastifyCookie);
+  app.setErrorHandler(httpErrorHandler);
 
   const routes = initControllers(container);
 
